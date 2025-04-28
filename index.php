64638c8849ebe610566329284c453a263fbf1c5c
dil-yo <71175689+dil-yo@users.noreply.github.com>
2025-04-28 13:31:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<body style="background-color:red;">
+<body style="background-color:green;">
 <marquee>
 <h1> Hello J2C Technologies.....!!!!!!!!! V2 Hello world </h1>
 </marquee>marquee>
