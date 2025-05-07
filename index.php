c43ff2e481ccb06b1d96db396bd710162d759793
dil-yo <71175689+dil-yo@users.noreply.github.com>
2025-05-07 16:43:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<body style="background-color:green;">
+<body style="background-color:blue;">
 <marquee>
 <h1> Welcome to Openshift World.....!!!!!!!!! App Version 1.0.0 </h1>
 </marquee>
